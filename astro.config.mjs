33e63809ce5e96cf9281f103aec9d40508590f83
OscarAB-Dev <oscar_amaral_bojorquez@hotmail.com>
2025-05-12 21:02:07 -0700

    Add auth-astro to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 import db from '@astrojs/db';
+import auth from 'auth-astro';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  integrations: [db()],
+  integrations: [db(), auth()],
+  vite: {plugins: [tailwindcss()]},
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: node({mode: "standalone"}),
+  
 });
